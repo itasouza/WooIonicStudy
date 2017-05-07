ca406ae9afcec88a9299147e7648f8ae969ad653
itasouza <itasouza@yahoo.com.br>
2017-05-07 16:00:30 -0300

    Versão Inicial - al 13
    instalação da api do ecommerce

---
+++ src/pages/home/home.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import * as WC from 'npm-woocommerce-api';
+
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+
+  WooCommerce: any;
+
+  constructor(public navCtrl: NavController) {
+  
+    this.WooCommerce = WC({
+      url:"http://wooionic.comajudacoletiva.com.br/",
+      consumerKey:"ck_899d1c4011cab2bde1f04cd52704c248f155ecdc",
+      consumerSecret:"cs_cc90feaf8467c0a53679be89f137557cdc7acbe5"
+    });
+
+    this.WooCommerce.getAsync('products').then((data) => {
+      console.log(data);
+    }, (err) =>{
+      console.log(err);
+    });
+
+
+
+  }
+
+}
