ffa31c939f3c300991e74bf3b328eb563769bbac
itasouza <itasouza@yahoo.com.br>
2017-05-07 16:11:03 -0300

    Adicionado opção para api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,7 +18,9 @@ export class HomePage {
     this.WooCommerce = WC({
       url:"http://wooionic.comajudacoletiva.com.br/",
       consumerKey:"ck_899d1c4011cab2bde1f04cd52704c248f155ecdc",
-      consumerSecret:"cs_cc90feaf8467c0a53679be89f137557cdc7acbe5"
+      consumerSecret:"cs_cc90feaf8467c0a53679be89f137557cdc7acbe5",
+      wp_api: true,
+      version: 'wc/v2'
     });
 
     this.WooCommerce.getAsync('products').then((data) => {
